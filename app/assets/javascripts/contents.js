70034f7020879f623180d75736b2bb657ef442c4
Alex <a.rymar@sumatosoft.com>
2018-04-06 17:04:33 +0300

    add codemirror for textarea

--- app/assets/javascripts/contents.js
+++ app/assets/javascripts/contents.js
@@ -1,8 +1,34 @@
 document.addEventListener('DOMContentLoaded', function(){
-    var editor = document.getElementsByTagName("textArea")[0]
-    CodeMirror.fromTextArea(editor, {
-        lineNumbers: true,
-        mode: "ruby",
-        theme: "rubyblue"
-    });
+    var editor = document.getElementById("code_area")
+    if (editor) {
+        CodeMirror.fromTextArea(editor, {
+            lineNumbers: true,
+            mode: "ruby",
+            theme: "rubyblue",
+            autoRefresh: true,
+            tabSize : 2,
+            readOnly: true
+        });
+    }
+
+    var content_body = document.getElementById("content_body")
+    if (content_body) {
+        CodeMirror.fromTextArea(content_body, {
+            lineNumbers: true,
+            mode: "ruby",
+            theme: "rubyblue",
+            autoRefresh: true,
+            tabSize : 2,
+        });
+    }
+    var lines = document.querySelectorAll(".CodeMirror-linenumber")
+    for (var i = 0; i < lines.length; i++) {
+        lines[i].onmouseenter = function() {
+            console.log(this);
+        }
+
+        lines[i].onmouseleave = function() {
+            console.log(this);
+        }
+    }
 }, false);
